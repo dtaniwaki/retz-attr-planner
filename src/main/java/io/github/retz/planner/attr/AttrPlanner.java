97b70a3bd9368f4ed57b06695cf5e8d00a01bc24
Tobias Pfeiffer <tgp@preferred.jp>
2017-03-22 14:18:47 +0900

    extend FIFOPlanner instead of RandomPlanner

--- src/main/java/io/github/retz/planner/attr/AttrPlanner.java
+++ src/main/java/io/github/retz/planner/attr/AttrPlanner.java
@@ -1,21 +1,6 @@
-/**
- *    Retz
- *    Copyright (C) 2017 Nautilus Technologies, Inc.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package io.github.retz.planner.attr;
 
+import io.github.retz.planner.builtin.FIFOPlanner;
 import io.github.retz.planner.spi.Plan;
 import io.github.retz.planner.spi.Planner;
 import io.github.retz.protocol.data.Job;
@@ -25,59 +10,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
-public class AttrPlanner implements Planner{
-    private static final Logger LOG = LoggerFactory.getLogger(AttrPlanner.class);
-    private static final List<String> ORDER_BY = Arrays.asList("id");
-    private Random random;
-    private int maxStock;
+public class AttrPlanner extends FIFOPlanner {
 
-    @Override
-    public void initialize(Properties p) throws Throwable {
-        random = new Random();
-    }
-
-    @Override
-    public boolean filter(Job job) {
-        // No job is removed from candidates
-        return true;
-    }
-
-    @Override
-    public void setMaxStock(int maxStock) {
-        LOG.info("setmaxstock = {}", maxStock);
-        this.maxStock = maxStock;
-    }
-
-    @Override
-    public void setUseGpu(boolean useGpu) {
-    }
-
-    @Override
-    public List<String> orderBy() {
-        return ORDER_BY;
-    }
-
-    @Override
-    public Plan plan(Map<String, ResourceQuantity> offers, List<Job> jobs) {
-        List<Job> queue = new LinkedList<>(jobs);
-        Plan plan = new Plan();
-        if (queue.size() == 0) {
-            for (Map.Entry<String, ResourceQuantity> entry : offers.entrySet()) {
-                if (plan.getOfferIdsToStock().size() < maxStock) {
-                    plan.addStock(entry.getKey());
-                }
-            }
-            return plan;
-        }
-        for (Map.Entry<String, ResourceQuantity> entry : offers.entrySet()) {
-            int i = random.nextInt(queue.size());
-            if (entry.getValue().fits(queue.get(i))) {
-                plan.setJob(entry.getKey(), queue.remove(i));
-            } else if (plan.getOfferIdsToStock().size() < maxStock) {
-                plan.addStock(entry.getKey());
-            }
-        }
-        LOG.info("Random plan: {}", plan);
-        return plan;
-    }
 }
