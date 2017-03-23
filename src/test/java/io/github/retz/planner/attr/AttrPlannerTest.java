62cfa8a88772b7e372b982dfb56cd19ba8abf01b
Tobias Pfeiffer <tgp@preferred.jp>
2017-03-23 13:55:10 +0900

    change tests to use new interface

--- src/test/java/io/github/retz/planner/attr/AttrPlannerTest.java
+++ src/test/java/io/github/retz/planner/attr/AttrPlannerTest.java
@@ -1,9 +1,10 @@
 package io.github.retz.planner.attr;
 
+import io.github.retz.planner.spi.Offer;
 import io.github.retz.planner.spi.Plan;
 import io.github.retz.planner.spi.Planner;
+import io.github.retz.planner.spi.Resource;
 import io.github.retz.protocol.data.Job;
-import io.github.retz.protocol.data.ResourceQuantity;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -14,16 +15,18 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 
 public class AttrPlannerTest {
-    Map<String, ResourceQuantity> mkOffers() {
-        Map<String, ResourceQuantity> offers = new HashMap<>();
-        offers.put("abc", new ResourceQuantity(2, 128, 0, 0, 555, 0));
-        offers.put("def", new ResourceQuantity(1, 256, 1, 0, 123, 0));
+    Map<String, Offer> mkOffers() {
+        Map<String, Offer> offers = new HashMap<>();
+        Resource abc = new Resource(2.0, 128, 555);
+        offers.put("abc", new Offer("abc", abc));
+        Resource def = new Resource(1.0, 256, 123);
+        offers.put("def", new Offer("def", def));
         return offers;
     }
 
     @Test
     public void firstMatch() {
-        Map<String, ResourceQuantity> offers = mkOffers();
+        Map<String, Offer> offers = mkOffers();
 
         // list of jobs, first job can be used with first offer
         List<Job> jobs = new LinkedList<>();
@@ -44,7 +47,7 @@ public class AttrPlannerTest {
 
     @Test
     public void simpleMatch() {
-        Map<String, ResourceQuantity> offers = mkOffers();
+        Map<String, Offer> offers = mkOffers();
 
         // list of jobs, first job can only be used with second offer
         List<Job> jobs = new LinkedList<>();
@@ -65,7 +68,7 @@ public class AttrPlannerTest {
 
     @Test
     public void noMatch() {
-        Map<String, ResourceQuantity> offers = mkOffers();
+        Map<String, Offer> offers = mkOffers();
 
         // list of jobs, no offer can satisfy requirements
         List<Job> jobs = new LinkedList<>();
